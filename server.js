eda239d7e041baf80d7bab0be525d881fa67f0ed
nitrogendragon <dirtboy4fun@gmail.com>
2020-01-11 17:39:35 -0600

    broken but proceeding towards getting things all set up

--- server.js
+++ server.js
@@ -3,11 +3,28 @@ if(process.env.NODE_ENV !== 'production'){
 }
 
 const stripeSecretKey = process.env.STRIPE_SECRET_KEY
-console.log(stripeSecretKey)
+const stripePublicKey = process.env.STRIPE_PUBLIC_KEY
+
+console.log(stripeSecretKey, stripePublicKey)
+
 const express = require('express')
 const app = express()/**sets up our express server */
+const fs = require('fs')
 
 app.set('view engine', 'ejs')/**tells the app to use ejs to render our views */
 app.use(express.static('public'))
+
+app.get('/store', function(req, res){
+    fs.readFile('items.json', function(error, data) {
+        if(error){
+            res.status(500).end()
+        }
+        else{
+            res.render('store.ejs', {
+                items: JSON.parse(data)
+            })
+        }
+    })
+})
 
 app.listen(3000)
